9101f5d85ac5e480777142272ebc298e8d00c8c8
Hussein Talal <hossientalal2@gmail.com>
2020-05-15 11:18:04 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,40 @@
+const routine = {
+
+
+
+  function wakeDog(dogName, dogBreed){
+      console.log(`Wake ${dogName} the ${dogBreed}`);
+      let msg = `Wake ${dogName} the ${dogBreed}`;
+      return msg;
+  }
+,
+  function leashDog(dogName, dogBreed){
+      console.log(`Leash ${dogName} the ${dogBreed}`);
+      let msg = `Leash ${dogName} the ${dogBreed}`;
+      return msg;
+  }
+,
+  function walkToPark(dogName, dogBreed){
+      console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+      let msg = `Walk to the park with ${dogName} the ${dogBreed}`;
+      return msg;
+  }
+,
+  function throwFrisbee(dogName, dogBreed){
+      console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+      let msg = `Throw the frisbee for ${dogName} the ${dogBreed}`;
+      return msg;
+  }
+,
+  function walkHome(dogName, dogBreed){
+      console.log(`Walk home with ${dogName} the ${dogBreed}`);
+      let msg = `Walk home with ${dogName} the ${dogBreed}`;
+      return msg;
+  }
+,
+  function unleashDog(dogName, dogBreed){
+      console.log(`Unleash ${dogName} the ${dogBreed}`);
+      let msg = `Unleash ${dogName} the ${dogBreed}`;
+      return msg;
+  }
+}
